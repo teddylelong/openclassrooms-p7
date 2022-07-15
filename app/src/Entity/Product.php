13d5984450987ccf99215b9f1791ca3f8bf0fd23
Teddy Lelong <teddy@webalgo.fr>
2022-07-15 18:13:38 +0200

    fix: added new properties to Product and solved SerializedName bug

--- app/src/Entity/Product.php
+++ app/src/Entity/Product.php
@@ -5,22 +5,26 @@ namespace App\Entity;
 use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\ProductRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\SerializedName;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ApiResource(
- *     collectionOperations={
-            "get"
- *     },
- *     itemOperations = {
-            "get"
- *     }
+ *     collectionOperations={"get"},
+ *     itemOperations = {"get"},
+ *     attributes={"force_eager"=false}
  * )
  *
  * @ORM\Entity(repositoryClass=ProductRepository::class)
  */
 class Product
 {
+    public function __construct()
+    {
+        $this->setCreatedAt(new \DateTimeImmutable('now'));
+        $this->setUpdatedAt(new \DateTimeImmutable('now'));
+    }
+
     /**
      * L'identifiant unique du produit
      *
@@ -102,22 +106,47 @@ class Product
      * @Assert\Range(
      *     min="1",
      *     max="9999999",
-     *     minMessage="Le prix doit être au moins supérieur ou égal à 1",
-     *     maxMessage="Le prix ne peut pas dépasser 9 999 999.99"
+     *     notInRangeMessage="Le prix du produit doit être compris entre 1,00 et 9 999 999,99"
      * )
      */
     private $price;
 
+    /**
+     * La date et l'heure de création du produit
+     *
+     * @ORM\Column(type="datetime_immutable")
+     * @SerializedName("createdAt")
+     */
+    private $created_at;
+
+    /**
+     * La date et l'heure de la dernière modification du produit
+     *
+     * @ORM\Column(type="datetime_immutable")
+     * @SerializedName("updatedAt")
+     */
+    private $updated_at;
+
+    /**
+     * @return int|null
+     */
     public function getId(): ?int
     {
         return $this->id;
     }
 
+    /**
+     * @return string|null
+     */
     public function getName(): ?string
     {
         return $this->name;
     }
 
+    /**
+     * @param string $name
+     * @return $this
+     */
     public function setName(string $name): self
     {
         $this->name = $name;
@@ -125,11 +154,18 @@ class Product
         return $this;
     }
 
+    /**
+     * @return string|null
+     */
     public function getBrand(): ?string
     {
         return $this->brand;
     }
 
+    /**
+     * @param string $brand
+     * @return $this
+     */
     public function setBrand(string $brand): self
     {
         $this->brand = $brand;
@@ -137,11 +173,18 @@ class Product
         return $this;
     }
 
+    /**
+     * @return string|null
+     */
     public function getDescription(): ?string
     {
         return $this->description;
     }
 
+    /**
+     * @param string $description
+     * @return $this
+     */
     public function setDescription(string $description): self
     {
         $this->description = $description;
@@ -149,14 +192,59 @@ class Product
         return $this;
     }
 
+    /**
+     * @return string|null
+     */
     public function getPrice(): ?string
     {
         return $this->price;
     }
 
+    /**
+     * @param string $price
+     * @return $this
+     */
     public function setPrice(string $price): self
     {
         $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTimeImmutable|null
+     */
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updated_at;
+    }
+
+    /**
+     * @param \DateTimeImmutable $updated_at
+     * @return $this
+     */
+    public function setUpdatedAt(\DateTimeImmutable $updated_at): self
+    {
+        $this->updated_at = $updated_at;
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTimeImmutable|null
+     */
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * @param \DateTimeImmutable $created_at
+     * @return $this
+     */
+    public function setCreatedAt(\DateTimeImmutable $created_at): self
+    {
+        $this->created_at = $created_at;
 
         return $this;
     }
