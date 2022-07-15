07893b4a59edd30526c7e2a764db1beda1b643d9
Teddy Lelong <teddy@webalgo.fr>
2022-07-15 17:30:41 +0200

    feat: added Asserts to Product + added APi infos into config/packages/api_platform.yaml

--- app/src/DataFixtures/AppFixtures.php
+++ app/src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Product;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +10,53 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $iPhone12 = (new Product())
+            ->setName('iPhone 12')
+            ->setBrand('Apple')
+            ->setDescription('As amazing as ever')
+            ->setPrice(599)
+        ;
+        $manager->persist($iPhone12);
+
+        $iPhone13 = (new Product())
+            ->setName('iPhone 13')
+            ->setBrand('Apple')
+            ->setDescription('A total powerhouse')
+            ->setPrice(699)
+        ;
+        $manager->persist($iPhone13);
+
+        $iPhone13Pro = (new Product())
+            ->setName('iPhone 13 Pro')
+            ->setBrand('Apple')
+            ->setDescription('The ultimate iPhone')
+            ->setPrice(999)
+        ;
+        $manager->persist($iPhone13Pro);
+
+        $iPhoneSE = (new Product())
+            ->setName('iPhone SE 2022')
+            ->setBrand('Apple')
+            ->setDescription('Serious power. Serious value.')
+            ->setPrice(429)
+        ;
+        $manager->persist($iPhoneSE);
+
+        $samsungS20 = (new Product())
+            ->setName('Galaxy S20')
+            ->setBrand('Samsung')
+            ->setDescription("Grand écran 6,5 pouces ultra fluide 120Hz")
+            ->setPrice(499)
+        ;
+        $manager->persist($samsungS20);
+
+        $samsungS22 = (new Product())
+            ->setName('Galaxy S22')
+            ->setBrand('Samsung')
+            ->setDescription("Ecran Dynamic AMOLED de 6,1 pouces pour une immersion totale dans vos contenus")
+            ->setPrice(799)
+        ;
+        $manager->persist($samsungS22);
 
         $manager->flush();
     }
--- app/src/Entity/Product.php
+++ app/src/Entity/Product.php
@@ -5,14 +5,25 @@ namespace App\Entity;
 use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\ProductRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
- * @ApiResource()
+ * @ApiResource(
+ *     collectionOperations={
+            "get"
+ *     },
+ *     itemOperations = {
+            "get"
+ *     }
+ * )
+ *
  * @ORM\Entity(repositoryClass=ProductRepository::class)
  */
 class Product
 {
     /**
+     * L'identifiant unique du produit
+     *
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
@@ -20,22 +31,80 @@ class Product
     private $id;
 
     /**
+     * Le nom du produit
+     *
      * @ORM\Column(type="string", length=255)
+     *
+     * @Assert\NotBlank(
+     *     message="Le nom du produit ne peut pas être vide"
+     * )
+     * @Assert\NotNull(
+     *     message="Le nom du produit ne peut pas être null"
+     * )
+     * @Assert\Length(
+     *     min=1,
+     *     max=255,
+     *     minMessage="Le nom doit contenir au moins {{ limit }} caratères",
+     *     maxMessage="Le nom ne doit pas dépasser {{ limit }} caractères"
+     * )
      */
     private $name;
 
     /**
+     * La marque du produit
+     *
      * @ORM\Column(type="string", length=255)
+     *
+     * @Assert\NotBlank(
+     *     message="La marque du produit ne peut pas être vide"
+     * )
+     * @Assert\NotNull(
+     *     message="La marque du produit ne peut pas être nulle"
+     * )
+     * @Assert\Length(
+     *     min=1,
+     *     max=255,
+     *     minMessage="La marque doit contenir au moins {{ limit }} caratères",
+     *     maxMessage="La marque ne doit pas dépasser {{ limit }} caractères"
+     * )
      */
     private $brand;
 
     /**
+     * La description du produit
+     *
      * @ORM\Column(type="text")
+     *
+     * @Assert\NotBlank(
+     *     message="La description du produit ne peut pas être vide"
+     * )
+     * @Assert\NotNull(
+     *     message="La description du produit ne peut pas être nulle"
+     * )
      */
     private $description;
 
     /**
+     * Le prix du produit (valeur numérique)
+     *
      * @ORM\Column(type="decimal", precision=7, scale=2)
+     *
+     * @Assert\NotBlank(
+     *     message="Le prix du produit ne peut pas être vide"
+     * )
+     * @Assert\NotNull(
+     *     message="Le prix du produit ne peut pas être nulle"
+     * )
+     * @Assert\Type(
+     *     type="Numeric",
+     *     message="Le prix doit être une valeur numérique"
+     * )
+     * @Assert\Range(
+     *     min="1",
+     *     max="9999999",
+     *     minMessage="Le prix doit être au moins supérieur ou égal à 1",
+     *     maxMessage="Le prix ne peut pas dépasser 9 999 999.99"
+     * )
      */
     private $price;
 
